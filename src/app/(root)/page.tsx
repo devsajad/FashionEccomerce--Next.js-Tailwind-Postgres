feb97a851e1da1458ec5542a8c33e0220a27bf8c
devsajad <sajadzarepur@gmail.com>
2025-07-12 19:06:19 +0330

    feat: implement product limit functionality in getProducts and update page component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,9 @@
 import ProductList from "@/components/shared/product/ProductList";
+import { LATES_PRODUCTS_LIMIT } from "@/lib/constants";
 import { getProducts } from "@/lib/data-service";
 
 export default async function page() {
-  const data = await getProducts();
-  return <ProductList title="جدیدترین محصولات" data={data} limit={4} />;
+  const data = await getProducts(LATES_PRODUCTS_LIMIT);
+
+  return <ProductList title="جدیدترین محصولات" data={data} />;
 }
--- src/lib/data-service.ts
+++ src/lib/data-service.ts
@@ -3,7 +3,11 @@ import { convertToPlainObject } from "./utils";
 
 const prisma = new PrismaClient();
 
-export async function getProducts() {
-  const products = await prisma.product.findMany();
+export async function getProducts(limit: number) {
+  const products = await prisma.product.findMany({
+    take: limit,
+    orderBy: { createdAt: "desc" },
+  });
+
   return convertToPlainObject(products);
 }
