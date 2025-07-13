18d78c123387cb06dacef03ff50f6dfcbfde2837
devsajad <sajadzarepur@gmail.com>
2025-07-13 13:41:14 +0330

    feat: update Prisma setup with Neon adapter and clean up schema

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-export const prisma = new PrismaClient().$extends({
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
     product: {
       rating: {
