abfd2ab5bbdfe05b4f75feb7e2a905e5a3cd3609
devsajad <sajadzarepur@gmail.com>
2025-07-13 19:47:57 +0330

    fix : move loading.js fallback component in (root) folder

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -4,15 +4,7 @@ import loader from "@/assets/loader.gif";
 
 export default function loading() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-      }}
-    >
+    <div className="w-full h-full flex items-center justify-center">
       <Image src={loader} width={70} height={70} alt="Loading..." />
     </div>
   );
